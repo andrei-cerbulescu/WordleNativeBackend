d7d18653d934fa622f7d32e5fc47fd91457ad144
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2022-04-23 17:46:44 +0300

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
-  before_action :validate_user
+  before_action :validate_user, except: [:create]
+
+  def create
+    username = params[:username]
+    password = params[:password]
+    unless username.match(/^[a-z0-9A-Z]+$/)
+      render json: {'error': 'Nume invalid'}, status: :not_found
+      return
+    end
+
+    if User.where(username: username).count > 0
+      render json: {'error': 'Utilizatorul deja există'}, status: :not_found
+      return
+    end
+    account = User.create(username: username, password: password)
+
+    if account.save
+      render json: {}, status: :ok
+      return
+    end
+    render json: {}, status: :not_found
+  end
 
   def get_current_user
     if @current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/word', to: 'words#get'
   post '/suggest_word', to: 'words#create'
-  post '/login', to: 'authentication#login'
   post '/rate_word', to: 'words#rate'
+
+  post '/login', to: 'authentication#login'
+
   get '/get_current_user', to: 'users#get_current_user'
+  post 'register', to: 'users#create'
 end
