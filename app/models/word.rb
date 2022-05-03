af4a844ae0d25850a9e1d5009a376421eeb69eed
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2022-05-03 20:40:30 +0300

    methods

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,10 @@
 class Word < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"
   validates :word, uniqueness: true
+
+  def rating
+    divide_by = (object.thumbs_up || 0) + (object.thumbs_down || 0)
+    divide_by = 1 if divide_by==0
+    (((object.thumbs_up || 0).to_f / divide_by).floor(4)*100).round(2)
+  end
 end
--- app/serializers/word_serializer.rb
+++ app/serializers/word_serializer.rb
@@ -2,8 +2,6 @@ class WordSerializer < ActiveModel::Serializer
   attributes :word, :thumbs_up, :thumbs_down, :id
 
   attribute :rating do
-    divide_by = (object.thumbs_up || 0) + (object.thumbs_down || 0)
-    divide_by = 1 if divide_by==0
-    (((object.thumbs_up || 0).to_f / divide_by).floor(4)*100).round(2)
+    object.rating
   end
 end
